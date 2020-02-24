900a9eaa61d8d3c612a04bb6716724b3bdf80395
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-02-24 13:39:47 -0500

    1/3 test cases pass

--- SistiMath.java
+++ SistiMath.java
@@ -2,7 +2,7 @@ package test;
 
 /**
 Author: Ferruccio Sisti
-Version 1.0 (No functions work, all test cases fail)
+Version 2.0 (Sin, toRadians and toDegrees functions work. 2/3 test cases fail)
 **/
 public class SistiMath
 {
@@ -23,19 +23,41 @@ public class SistiMath
 		//Function converts radians to degrees
 		public double radToDegrees(double x)
 		{
-			return 6000.83;
+			return (x * 180 / 3.14157);
 		}
 
 		//Function converts degrees to radians
 		public double degreeToRad(double x)
 		{
-			return 6000.83;
+			return (x * 3.14157 / 180);
 		}
 
 		//Function calculates the sin(x is given in radians)
 		public double sistiSin(double x)
 		{
-			return 6000.83;
+			// convert x to an angle between -2 PI and 2 PI
+	        x = x % (2 * 3.14157);
+
+	        // compute the Taylor series approximation
+	        double term = 1.0;      // ith term = x^i / i!
+	        double ans  = 0.0;      // sum of first i terms in taylor series
+
+	        //Keep looping until terms are too small to make a difference
+	        //Alternate adding and subtracting, and calculate new term without having
+	        //To actually use pow and factorial functions
+	        for (int i = 1; term != 0.0; i++)
+	        {
+	            term *= (x / i);
+	            if (i % 4 == 1)
+	            	{
+	            		ans += term;
+	            	}
+	            if (i % 4 == 3)
+	            	{
+	            		ans -= term;
+	            	}
+	        }
+	        return ans;
 		}
 
 		//Function calculates the cos(x is given in radians)
@@ -53,7 +75,7 @@ public class SistiMath
 		//Function calculates the sin(x is given in degrees)
 		public double sistiSinDeg(double x)
 		{
-			return 6000.83;
+			return sistiSin(degreeToRad(x));
 		}
 
 		//Function calculates the cos(x is given in degrees)
