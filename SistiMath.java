7595ac7a0bafbb0516083ffc5cf51450dd50e554
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-02-24 13:37:37 -0500

    All test cases fail

---
+++ SistiMath.java
@@ -0,0 +1,71 @@
+package test;
+
+/**
+Author: Ferruccio Sisti
+Version 1.0 (No functions work, all test cases fail)
+**/
+public class SistiMath
+{
+	public SistiMath()
+	{
+		//Empty default constructor
+	}
+
+	public static class Trig
+	{
+
+		public Trig()
+		{
+			//Empty default constructor
+			//Don't need anything in here for the trig functions to work
+		}
+
+		//Function converts radians to degrees
+		public double radToDegrees(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function converts degrees to radians
+		public double degreeToRad(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function calculates the sin(x is given in radians)
+		public double sistiSin(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function calculates the cos(x is given in radians)
+		public double sistiCos(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function calculates the tan(x is given in radians)
+		public double sistiTan(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function calculates the sin(x is given in degrees)
+		public double sistiSinDeg(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function calculates the cos(x is given in degrees)
+		public double sistiCosDeg(double x)
+		{
+			return 6000.83;
+		}
+
+		//Function calculates the tan(x is given in degrees)
+		public double sistiTanDeg(double x)
+		{
+			return 6000.83;
+		}
+	}
+}
